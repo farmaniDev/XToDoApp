bdd9a28abe05b614c3a7280d400a2df21f19c13f
Mohammad <techwithfarmani@gmail.com>
2023-01-26 18:11:50 +0330

    Add Fragment UI

--- app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
@@ -1,11 +1,14 @@
 package com.farmani.xtodo.fragments.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.farmani.xtodo.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
 
 class ListFragment : Fragment() {
     override fun onCreateView(
@@ -13,7 +16,13 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_list, container, false)
+        view.fab_add.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
     }
+
 
 }
