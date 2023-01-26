315c92c170924b3c6e7d34c0422ab7c3d694ee24
Mohammad <techwithfarmani@gmail.com>
2023-01-26 19:42:52 +0330

    Menu - Part 1

--- app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
@@ -1,16 +1,17 @@
 package com.farmani.xtodo.fragments.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.farmani.xtodo.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), MenuProvider {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,8 +22,16 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return view
     }
 
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.main_menu, menu)
+    }
 
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
+    }
 }
