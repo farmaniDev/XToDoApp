d678117a24d624eeb633b29b3e79d04eb3ee3ff2
Mohammad <techwithfarmani@gmail.com>
2023-01-26 10:34:30 +0330

    Navigation Fragment

--- app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/farmani/xtodo/fragments/list/ListFragment.kt
@@ -1,19 +1,19 @@
-package com.farmani.xtodo
+package com.farmani.xtodo.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.farmani.xtodo.R
 
-class AddFragment : Fragment() {
-
+class ListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
 }
